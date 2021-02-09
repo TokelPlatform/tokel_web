4ca9d0751fd319317c3dde4a4d49e966d11a10fc
piggydoughnut <drmikhailova@gmail.com>
2021-02-09 17:29:27 +1300

    added icons

---
+++ src/data/icons.js
@@ -0,0 +1,25 @@
+import github from "../images/icons/icon_github.svg"
+import discord from "../images/icons/icon_discord.svg"
+import coinWallet from "../images/icons/icon_coin_wallet.svg"
+import dex from "../images/icons/icon_dex.svg"
+import donate from "../images/icons/icon_donate.svg"
+import explorer from "../images/icons/icon_explorer.svg"
+import heart from "../images/icons/icon_heart.svg"
+import market from "../images/icons/icon_market.svg"
+import tokenWallet from "../images/icons/icon_token_wallet.svg"
+import twitter from "../images/icons/icon_twitter.svg"
+
+const icons = {
+  discord,
+  github,
+  coinWallet,
+  dex,
+  donate,
+  explorer,
+  heart,
+  market,
+  tokenWallet,
+  twitter
+}
+
+export default icons
