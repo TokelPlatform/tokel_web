c7b239c76300a4d46ead47dc28afb2108e9a9f48
piggydoughnut <drmikhailova@gmail.com>
2021-02-11 18:34:51 +1300

    added dark icons

--- src/data/icons.js
+++ src/data/icons.js
@@ -1,5 +1,7 @@
 import github from "../images/icons/icon_github.svg"
+import github_dark from "../images/icons/icon_github_dark.svg"
 import discord from "../images/icons/icon_discord.svg"
+import discord_dark from "../images/icons/icon_discord_dark.svg"
 import coinWallet from "../images/icons/icon_coin_wallet.svg"
 import dex from "../images/icons/icon_dex.svg"
 import donate from "../images/icons/icon_donate.svg"
@@ -8,10 +10,13 @@ import heart from "../images/icons/icon_heart.svg"
 import market from "../images/icons/icon_market.svg"
 import tokenWallet from "../images/icons/icon_token_wallet.svg"
 import twitter from "../images/icons/icon_twitter.svg"
+import twitter_dark from "../images/icons/icon_twitter_dark.svg"
 
 const icons = {
   discord,
+  discord_dark,
   github,
+  github_dark,
   coinWallet,
   dex,
   donate,
@@ -19,7 +24,8 @@ const icons = {
   heart,
   market,
   tokenWallet,
-  twitter
+  twitter,
+  twitter_dark
 }
 
 export default icons
