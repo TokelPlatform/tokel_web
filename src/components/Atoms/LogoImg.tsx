10ca3e86640113d4d0bd195302765ac910c99eb1
piggydoughnut <drmikhailova@gmail.com>
2021-02-11 16:59:31 +1300

    logo image returns an open and closed logo

--- src/components/Atoms/LogoImg.tsx
+++ src/components/Atoms/LogoImg.tsx
@@ -1,13 +1,23 @@
 import React, { ReactElement } from "react"
 import styled from "@emotion/styled"
 import logo from "../../images/logo.svg"
+import openLogo from "../../images/logo-open.svg"
 
+type LogoImgProps  = {
+    open: boolean,
+    width: string
+}
+const defaultProps = {
+    open: false,
+    width: '24px'
+}
 const Logo = styled.img`
-    width: 24px;
+    width: ${props => props.width};
 `
 
-const LogoImg = (): ReactElement => (
-    <Logo src={logo}></Logo>
+const LogoImg = ({width, open}: LogoImgProps): ReactElement => (
+    <Logo width={width} src={open ? openLogo : logo}/>
 )
 
+LogoImg.defaultProps = defaultProps
 export default LogoImg
