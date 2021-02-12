cf49b7d316a94e9df9864b365cced41918473859
piggydoughnut <drmikhailova@gmail.com>
2021-02-12 18:32:29 +1300

    little hack for links

--- src/data/links.js
+++ src/data/links.js
@@ -1,6 +1,10 @@
 const links = {
     discord: 'https://discord.gg/DnajQwps',
+    discord_dark: 'https://discord.gg/DnajQwps',
+    twitter: 'https://twitter.com',
+    twitter_dark: 'https://twitter.com',
     github: 'https://github.com/TokelPlatform/tokel_app',
+    github_dark: 'https://github.com/TokelPlatform/tokel_app',
     openseaImage: 'https://opensea.io/assets/0xc2c747e0f7004f9e8817db2ca4997657a7746928/8064',
     superrareImage: 'https://superrare.co/artwork-v2/cochleanaut-18746'
   }
