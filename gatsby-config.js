17b8801db2c85f896d1e7f906ffcf44d47779d65
piggydoughnut <drmikhailova@gmail.com>
2021-07-21 16:56:34 +0800

    added matomo

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,14 @@ module.exports = {
           },
         ]
       },
+    },
+    {
+      resolve: 'gatsby-plugin-matomo',
+      options: {
+        siteId: '1',
+        matomoUrl: 'https://analytics.tokel.io/matomo.php',
+        siteUrl: 'https://tokel.io'
+      }
     }
     
     
