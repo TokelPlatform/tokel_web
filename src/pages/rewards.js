8ae86161374a3c8c36abf254c02d864d62a6660b
piggydoughnut <drmikhailova@gmail.com>
2021-09-28 14:35:17 +0800

    wording change

--- src/pages/rewards.js
+++ src/pages/rewards.js
@@ -1,10 +1,10 @@
-import * as React from "react"
+import * as React from 'react';
 
-import PageRootContainer from "./template"
+import PageRootContainer from './template';
 import axios from 'axios';
-import breakpoints from "../styles/breakpoints"
-import links from "../data/links"
-import styled from "@emotion/styled"
+import breakpoints from '../styles/breakpoints';
+import links from '../data/links';
+import styled from '@emotion/styled';
 
 const RewardsRoot = styled.div`
   text-align: center;
@@ -41,14 +41,14 @@ const RewardsRoot = styled.div`
     }
   }
 } 
-`
+`;
 
 const ResultBlock = styled.div`
   text-align: center;
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+`;
 
 const Label = styled.h3`
   font-size: 18px;
@@ -58,13 +58,13 @@ const Label = styled.h3`
   text-transform: uppercase;
   margin-bottom: 0;
   @media (max-width: ${breakpoints.mobile}) {
-    font-size: 16px;      
+    font-size: 16px;
   }
 
   /* Base/slate */
 
-  color: #7C91A7;
-`
+  color: #7c91a7;
+`;
 const Result = styled.p`
   font-size: 24px;
   line-height: 31px;
@@ -75,17 +75,17 @@ const Result = styled.p`
   color: white;
   margin-top: 10px;
   @media (max-width: ${breakpoints.mobile}) {
-    font-size: 18px;      
+    font-size: 18px;
   }
-`
+`;
 
 const Submit = styled.button`
   width: 155px;
   height: 60px;
   cursor: pointer;
 
-  background: #F0F0F0;
-  border: 1px solid #DEDEDE;
+  background: #f0f0f0;
+  border: 1px solid #dedede;
   box-sizing: border-box;
   font-size: 20px;
   color: #000000;
@@ -93,7 +93,7 @@ const Submit = styled.button`
   @media (max-width: ${breakpoints.mobile}) {
     margin-top: 10px;
   }
-`
+`;
 
 const Input = styled.input`
   width: 350px;
@@ -104,52 +104,53 @@ const Input = styled.input`
 
   font-family: 'ibm_plex', monospace;
 
-  border: 1px solid #DEDEDE;
+  border: 1px solid #dedede;
   box-sizing: border-box;
   background-color: transparent;
-`
+`;
 
 const Error = styled.div`
   height: 20px;
   margin-bottom: 20px;
-  color: #CE395F;
-`
+  color: #ce395f;
+`;
 
 const Note = styled.div`
   color: white;
   height: 20px;
-`
+`;
 
 const SectionHeader = styled.h3`
-    color: white;
-    opacity: 0.8;
-    margin-bottom: 0px;
-    @media (max-width: ${breakpoints.mobile}) {
-        text-align: center;
-    } 
-`
+  color: white;
+  opacity: 0.8;
+  margin-bottom: 0px;
+  @media (max-width: ${breakpoints.mobile}) {
+    text-align: center;
+  }
+`;
 
 const Section = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 600px;
-    margin-top: 45px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 600px;
+  margin-top: 45px;
+  text-align: left;
+  @media (max-width: ${breakpoints.tablet}) {
+    width: 400px;
+  }
+  @media (max-width: ${breakpoints.mobile}) {
+    width: 100%;
     text-align: left;
-    @media (max-width: ${breakpoints.tablet}) {
-        width: 400px;
-    }
-    @media (max-width: ${breakpoints.mobile}) {
-        width: 100%;
-        text-align: left;
-    }   
-    p {
-      color: white;
-      opacity: 0.8;
-    }
-`
+  }
+  p {
+    color: white;
+    opacity: 0.8;
+  }
+`;
 
-export const isAddressValid = (address) => /^[a-km-zA-HJ-NP-Z1-9]{26,35}$/.test(address);
+export const isAddressValid = (address) =>
+  /^[a-km-zA-HJ-NP-Z1-9]{26,35}$/.test(address);
 
 const Rewards = () => {
   const [address, setAddress] = React.useState(null);
@@ -158,86 +159,94 @@ const Rewards = () => {
   const [rewards2, setRewards2] = React.useState(null);
   const [error, setError] = React.useState(null);
   const [fetching, setFetching] = React.useState(null);
-  const [success, setSuccess]= React.useState(null);
-  
+  const [success, setSuccess] = React.useState(null);
+
   const getData = (address) => {
     setSuccess(false);
-    setError(null)
-    setFetching('Fetching data...')
-   
-  if(!address || address.length < 26 || !isAddressValid(address)) {
-    setError('Invalid address');
-    setFetching(null);
-    return;
-  }
-  return axios(`https://dexstats.info/api/rewardstokel.php?address=${address}`)
-    .then(res => {  
+    setError(null);
+    setFetching('Fetching data...');
+
+    if (!address || address.length < 26 || !isAddressValid(address)) {
+      setError('Invalid address');
+      setFetching(null);
+      return;
+    }
+    return axios(
+      `https://dexstats.info/api/rewardstokel.php?address=${address}`
+    )
+      .then((res) => {
         if (res.data) {
-          setSuccess(true)
+          setSuccess(true);
           setFetching(null);
           setBalance(res.data.balance);
-          setRewards1(res.data.FirstUnlock)
-          setRewards2(res.data.SecondUnlock)
+          setRewards1(res.data.FirstUnlock);
+          setRewards2(res.data.SecondUnlock);
         }
-    })
-    .catch(e => {
-      setSuccess(false);
-      setError(e.message);
-      setFetching(null);
-    });
-     
-  }
+      })
+      .catch((e) => {
+        setSuccess(false);
+        setError(e.message);
+        setFetching(null);
+      });
+  };
 
   const handleChange = (val) => {
     setAddress(val);
     setBalance(null);
-    setRewards1(null)
-    setRewards2(null)
-  }
+    setRewards1(null);
+    setRewards2(null);
+  };
 
   return (
     <PageRootContainer>
       <RewardsRoot>
         <h1>Early adopter rewards initiative</h1>
-        <a href={links.mediumEarlyAdopters}><h2>Read about TKL time lock rewards here</h2></a>
-    <div style={{marginTop: '30px'}}>
-      <div style={{height: '20px'}}>
-        {error ? <Error>{error}</Error>  : <Note>{fetching}</Note>} 
-      </div>
-      <Note>
-      </Note>
-      <Input placeholder="Your TKL address"onChange={val => handleChange(val.target.value)}></Input>
-      <Submit onClick={() => getData(address)}> Submit </Submit>
-    </div>
-        {success &&
-        <div style={{marginTop: '30px'}}>
-          <ResultBlock>
-            <Label>Address</Label>
-            <Result>{address}</Result>
-          </ResultBlock>
-          <ResultBlock>
-            <Label>current balance</Label>
-            <Result>{Number(balance)} TKL</Result>
-          </ResultBlock>
-          <ResultBlock>
-            <Label>Rewards unlockable 15th June 2022</Label>
-            <Result>{rewards1} TKL</Result>
-          </ResultBlock>
-          <ResultBlock>
-            <Label>Rewards unlockable 15th March 2023</Label>
-            <Result>{rewards2} TKL</Result>
-          </ResultBlock>
+        <a href={links.mediumEarlyAdopters}>
+          <h2>Read about TKL time lock rewards here</h2>
+        </a>
+        <div style={{ marginTop: '30px' }}>
+          <div style={{ height: '20px' }}>
+            {error ? <Error>{error}</Error> : <Note>{fetching}</Note>}
+          </div>
+          <Note></Note>
+          <Input
+            placeholder="Your TKL address"
+            onChange={(val) => handleChange(val.target.value)}
+          ></Input>
+          <Submit onClick={() => getData(address)}> Submit </Submit>
         </div>
-        }
+        {success && (
+          <div style={{ marginTop: '30px' }}>
+            <ResultBlock>
+              <Label>Address</Label>
+              <Result>{address}</Result>
+            </ResultBlock>
+            <ResultBlock>
+              <Label>final balance on 15.09.21 8pm</Label>
+              <Result>{Number(balance)} TKL</Result>
+            </ResultBlock>
+            <ResultBlock>
+              <Label>Rewards unlockable 15th June 2022</Label>
+              <Result>{rewards1} TKL</Result>
+            </ResultBlock>
+            <ResultBlock>
+              <Label>Rewards unlockable 15th March 2023</Label>
+              <Result>{rewards2} TKL</Result>
+            </ResultBlock>
+          </div>
+        )}
         <Section>
-          <SectionHeader>
-              DISCLAIMER
-          </SectionHeader>
-          <p>This rewards calculator uses current conditions to populate these estimations. These estimations should be used as a guide based on the current amount of TKL sold, and is in no way a guarantee that you will eventually receive this many coins.</p>
-      </Section> 
+          <SectionHeader>DISCLAIMER</SectionHeader>
+          <p>
+            This rewards calculator uses current conditions to populate these
+            estimations. These estimations should be used as a guide based on
+            the current amount of TKL sold, and is in no way a guarantee that
+            you will eventually receive this many coins.
+          </p>
+        </Section>
       </RewardsRoot>
     </PageRootContainer>
-  )
-}
+  );
+};
 
-export default Rewards
+export default Rewards;
