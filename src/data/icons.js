72b8cb6fb790ea578eee282fd0f3b0312bbbb04a
piggydoughnut <piggydoughnut@gmail.com>
2022-02-22 13:00:23 +0800

    removed unused icons, added new icons

--- src/data/icons.js
+++ src/data/icons.js
@@ -1,45 +1,39 @@
-import coinWallet from "../images/icons/icon_coin_wallet.svg"
-import dex from "../images/icons/icon_dex.svg"
-import discord from "../images/icons/icon_discord.svg"
-import discord_dark from "../images/icons/icon_discord_dark.svg"
-import document from "../images/icons/icon_document.svg"
-import donate from "../images/icons/icon_donate.svg"
-import explorer from "../images/icons/icon_explorer.svg"
-import flote from "../images/icons/icon_flote.svg"
-import github from "../images/icons/icon_github.svg"
-import github_dark from "../images/icons/icon_github_dark.svg"
-import heart from "../images/icons/icon_heart.svg"
-import market from "../images/icons/icon_market.svg"
-import medium from "../images/icons/icon_medium.svg"
-import reddit from "../images/icons/icon_reddit.svg"
-import telegram from "../images/icons/icon_telegram.svg"
-import telegram_dark from "../images/icons/icon_telegram_dark.svg"
-import tokenWallet from "../images/icons/icon_token_wallet.svg"
-import twitter from "../images/icons/icon_twitter.svg"
-import twitter_dark from "../images/icons/icon_twitter_dark.svg"
-import youtube from "../images/icons/icon_youtube.svg"
+import discord from "images/icons/discord.svg"
+import document from "images/icons/icon_document.svg"
+import flote from "images/icons/flote.svg"
+import github from "images/icons/github.svg"
+import medium from "images/icons/medium.svg"
+import reddit from "images/icons/reddit.svg"
+import telegram from "images/icons/telegram.svg"
+import twitter from "images/icons/twitter.svg"
+import youtube from "images/icons/youtube.svg"
+import amazonMusic from 'images/icons/amazon-music.svg'
+import applePodcasts from 'images/icons/apple-podcasts.svg'
+import deezer from 'images/icons/deezer.svg'
+import facebook from 'images/icons/facebook.svg'
+import googlePodcasts from 'images/icons/google-podcasts.svg'
+import instagram from 'images/icons/instagram.svg'
+import spotify from 'images/icons/spotify.svg'
+import stitcher from 'images/icons/stitcher.svg'
 
 const icons = {
+  amazonMusic,
+  applePodcasts,
+  deezer,
   discord,
-  discord_dark,
-  github,
-  github_dark,
-  coinWallet,
-  dex,
-  donate,
-  explorer,
-  heart,
-  market,
-  tokenWallet,
-  twitter,
-  twitter_dark,
   document,
-  telegram,
-  telegram_dark,
-  youtube,
-  reddit,
+  facebook,
+  flote,
+  github,
+  googlePodcasts,
+  instagram,
   medium,
-  flote
+  reddit,
+  spotify,
+  stitcher,
+  telegram,
+  twitter,
+  youtube,
 }
 
 export default icons
