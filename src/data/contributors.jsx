7f8dc6c63ebcad2f11e4d82693007088ab3352f1
piggydoughnut <piggydoughnut@gmail.com>
2022-05-10 10:38:56 +0800

    text change

--- src/data/contributors.jsx
+++ src/data/contributors.jsx
@@ -24,7 +24,7 @@ export default [
     },
     {
         name:'Daria',
-        position: 'Co-founder & Development lead',
+        position: 'Co-founder & Senior Software Engineer',
         image: 'daria',
         socials: [
             {
